c399173e799013fbbbf61ab6e83c4348b521d093
MK <fengmk2@gmail.com>
2026-01-06 19:34:58 +0800

    FIXUP

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from '@voidzero-dev/vite-plus';
 
 export default defineConfig({
 	test: {
-		projects: ['./packages/happy-dom/vitest.config.ts'],
-		testTimeout: 10000,
+		projects: ['./packages/happy-dom/vitest.config.ts']
 	}
 });
